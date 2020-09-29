5b3fed2a6188af60fa388ef84ebd665f447f9527
CheesyBC <49570924+CheesyBC@users.noreply.github.com>
2020-09-29 02:54:48 +0000

    This is a script for minecraft console client
    https://ci.appveyor.com/project/ORelio/minecraft-console-client/build/artifacts
    Add the following code in your matches.ini (replacing the @ with your bot prefix)
    [Match]
    regex=^.*@ping ([a-zA-Z0-9_]+).*$
    action=script ping $1

---
+++ ping.cs
@@ -0,0 +1,35 @@
+//MCCScript 1.0
+if(args.Length == 0) {
+	MCC.LoadBot(new PingBot());
+} else {
+	MCC.LoadBot(new PingBot(args[0]));
+}
+
+//MCCScript Extensions
+
+public class PingBot : ChatBot {
+    private string player = "";
+
+    public PingBot(string pl) {
+        this.player = pl;
+    }
+
+    public PingBot() {
+	this.player = "";
+    }
+
+    public override void Initialize() {
+        if(this.player == "") {
+            this.player = GetUsername();
+        }
+        foreach(var e in GetPlayersLatency()) {
+            if (String.Equals(this.player, e.Key, StringComparison.OrdinalIgnoreCase)) {
+                SendText(this.player + "'s ping is " + e.Value);
+            }
+
+        }
+    }
+}
+
+//Developed primarily by crepppy
+//For CheesyBC
